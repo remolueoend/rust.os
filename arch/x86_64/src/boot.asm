dceb94a193d50ac2403c0274441b67e8418c5169
Remo Zumsteg <remolueoend@noreply.com>
2017-09-24 16:42:46 +0200

    Implement 64bit CPU mode

--- arch/x86_64/src/boot.asm
+++ arch/x86_64/src/boot.asm
@@ -1,4 +1,5 @@
 global start
+extern long_mode_start
 
 section .text
 bits 32
@@ -9,8 +10,12 @@ start:
   call check_cpuid
   call check_long_mode
 
-  mov dword [0xb8000], 0x2f4b2f4f
-  hlt
+  call setup_page_tables
+  call enable_paging
+
+  ; load the 64bit GDT:
+  lgdt [gdt64.pointer]
+  jmp gdt64.code:long_mode_start
 
 check_multiboot:
   cmp eax, 0x36d76289
@@ -73,6 +78,55 @@ check_long_mode:
   mov al, "2"
   jmp error
 
+setup_page_tables:
+  ; map first p4 entry to p3 table:
+  mov eax, p3_table
+  or eax, 0b11 ; present + writable
+  mov [p4_table], eax
+
+  ; map first p3 entry to p2 table:
+  mov eax, p2_table
+  or eax, 0b11
+  mov [p3_table], eax
+
+  ; map each p2 entry to a 2Mib page:
+  mov ecx, 0         ; counter variable
+
+.map_p2_table:
+  ; map ecx-th P2 entry to a huge page that starts at 2mib*ecx
+  mov eax, 0x200000  ; 2MiB
+  mul ecx            ; start address of ecx-th page
+  or eax, 0b10000011 ; present + writable + huge
+  mov [p2_table + ecx * 8], eax
+  inc ecx
+  cmp ecx, 512
+  jne .map_p2_table
+
+  ret
+
+enable_paging:
+  ; load p4 to cr3 register (cpu looks there for page table address):
+  mov eax, p4_table
+  mov cr3, eax
+
+  ; enable pae-flag in cr4 (physical address extension):
+  mov eax, cr4
+  or eax, 1 << 5
+  mov cr4, eax
+
+  ; set the long mode bit in the EFER MSR (model specific register):
+  mov ecx, 0xC0000080
+  rdmsr
+  or eax, 1 << 8
+  wrmsr
+
+  ; enable paging in the cr0 register:
+  mov eax, cr0
+  or eax, 1 << 31
+  mov cr0, eax
+
+  ret
+
 error:
   mov dword [0xb8000], 0x4f524f45
   mov dword [0xb8004], 0x4f3a4f52
@@ -81,7 +135,22 @@ error:
   hlt
 
 section .bss
+align 4096
+p4_table:
+  resb 4096
+p3_table:
+  resb 4096
+p2_table:
+  resb 4096
 stack_bottom:
   resb 64
 stack_top
 
+section rodata
+gdt64:
+  dq 0 ; zero entry
+.code: equ $ - gdt64
+  dq (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53) ; code segment
+.pointer:
+  dw $ - gdt64 - 1
+  dq gdt64
