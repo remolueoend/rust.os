2bc499c74874d33a060feba93dbd737a7a220907
Remo Zumsteg <remolueoend@noreply.com>
2017-11-05 22:54:52 +0100

    Add initial call to Rust from asm

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 #![feature(lang_items)]
 #![no_std]
 
+extern crate rlibc;
+
 #[no_mangle]
 pub extern fn rust_main() {
+  // let x = ["hello", "world", "!"];
+  // let y = x;
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
