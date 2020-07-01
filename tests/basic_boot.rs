f20588536aa8811d4e7485afb63edc7b1b1f51fc
remolueoend <remolueoend@users.noreply.github.com>
2020-07-01 13:40:41 +0200

    implement chapter: unit and integration tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,4 +1,3 @@
-// disable standard library for our own OS:
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
@@ -6,27 +5,14 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use rust_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World, it's {} time!", 1.337);
-    println!("aaaaand again!");
-
-    #[cfg(test)]
     test_main();
-
+    
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    println!("{}", _info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     rust_os::test_panic_handler(_info)
